67e3e47500b92144430b700d4fc8014586445377
Matus Goljer <matus.goljer@gmail.com>
2017-05-27 18:29:29 +0200

    Conform to base-build conventions

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-export TERM="xterm"
-
-cd /girara && make install && make install
-cd /zathura && make install
-cd /zathura-pdf-poppler && make install
-cd /zathura-djvu && make install
-cd /zathura-ps && make install
-cd /zathura-cb && make install
+__DIR__=$(dirname $(readlink -f "$0"))
+[[ ! -s ~/bin/zathura ]] && ln -s ${__DIR__}/zathura.sh ~/bin/zathura
